22bfbbbdbcef0ea6c5f297954376e855499ec964
Joy Ahmed <dev.joyahmed@gmail.com>
2024-04-16 01:31:01 +0600

    added upload files func

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataentry',
+    'uploads'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media file conf
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
