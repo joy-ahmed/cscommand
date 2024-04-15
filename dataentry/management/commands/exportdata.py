14ab54dbf673d1c4af7dd65052ce828a067c3cf1
Joy Ahmed <dev.joyahmed@gmail.com>
2024-04-15 17:47:35 +0600

    export any model from db

--- dataentry/management/commands/exportdata.py
+++ dataentry/management/commands/exportdata.py
@@ -1,4 +1,5 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.apps import apps
 import csv
 import os
 import datetime
@@ -8,9 +9,26 @@ from dataentry.models import *
 class Command(BaseCommand):
     help = 'Export data from database to csv'
 
+    def add_arguments(self, parser):
+        parser.add_argument('model_name', type=str, help='Model name')
+
     def handle(self, *args, **kwargs):
-        # fetch data from database
-        students = Student.objects.all()
+        model_name = kwargs['model_name']
+
+        model = None
+        # loop through all the models based on argument
+        for app_config in apps.get_app_configs():
+            try:
+                model = app_config.get_model(model_name)
+                break
+            except LookupError:
+                continue
+
+        if not model:
+            raise CommandError(f"Model '{model_name}' not found")
+
+         # fetch data from database
+        data = model.objects.all()
 
         # define the csv file name/path
         file_name = f"export-{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
@@ -21,10 +39,11 @@ class Command(BaseCommand):
         with open(file_path, 'w', newline='') as file:
             writer = csv.writer(file)
             # write header
-            writer.writerow(['Roll No', 'Name', 'Age'])
+            writer.writerow([field.name for field in model._meta.fields])
 
             # write data rows
-            for student in students:
-                writer.writerow([student.roll_no, student.name, student.age])
+            for record in data:
+                writer.writerow([getattr(record, field.name)
+                                for field in model._meta.fields])
 
         self.stdout.write(self.style.SUCCESS('Data exported successfully'))
