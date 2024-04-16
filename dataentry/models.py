ed14d54e9ff913b3a2066dc00a350003f2280382
Joy Ahmed <dev.joyahmed@gmail.com>
2024-04-17 00:34:59 +0600

    added employee model

--- dataentry/models.py
+++ dataentry/models.py
@@ -18,3 +18,17 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.customer_name
+
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=10)
+    employee_name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    retirement = models.DecimalField(max_digits=10, decimal_places=2)
+    other_benifits = models.DecimalField(max_digits=10, decimal_places=2)
+    total_benifits = models.DecimalField(max_digits=10, decimal_places=2)
+    total_compensation = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.employee_name} - {self.designation}'
